69b163afd86ecdaff5ded9966c4b1b3a70e34289
Marcello <oliv.marq.marcello@gmail.com>
2020-09-29 16:47:24 -0300

    update parser

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ export const removeMermaidGraph = doc => {
   const graphEndIndex = doc.lastIndexOf('```') + 3;
   if (graphInitIndex === -1 || graphEndIndex === -1) return doc;
   return doc.substr(0, graphInitIndex) + doc.substr(graphEndIndex);
-}
-
+ }
+  
 export const addPropertiesSubtitle = doc => {
-  const eventsIndex = doc.indexOf('## Events');
-  const dependenciesIndex = doc.indexOf('## Dependencies');
-  const footerIndex = doc.indexOf('----------------------------------------------');
-
-  let propertySubtitleIndex = footerIndex - 1;
-  if (eventsIndex !== -1) propertySubtitleIndex = eventsIndex - 1;
-  else if (dependenciesIndex !== -1) propertySubtitleIndex = dependenciesIndex - 1;
-
-  const propertiesSubtitle = `
-  ##### [*****] required
-  `;
-  return doc.substring(0, propertySubtitleIndex) + propertiesSubtitle + doc.substring(propertySubtitleIndex);
+  return doc.toString().replace(`## Properties`, `## Properties\n##### [*] required`);
 }
-
+  
 export const format = doc =>
   addPropertiesSubtitle(removeMermaidGraph(doc));
